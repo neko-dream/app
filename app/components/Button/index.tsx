f96dd19fa05065a2e1c4f34243a6c25424e59f31
RIKKAEBI <135724197+RIKKAEBI@users.noreply.github.com>
2024-09-28 17:10:03 +0000

    🚧 link button #3

--- app/components/Button/index.tsx
+++ app/components/Button/index.tsx
@@ -1,14 +1,20 @@
+import { Link, RemixLinkProps } from "@remix-run/react/dist/components";
 import { ComponentProps, ForwardedRef, forwardRef, ReactNode } from "react";
 import { OpinionBgColorMap } from "~/constants/opinion";
 
-type Props = Button & {
+type Props = (Button | Link) & {
+  variation: keyof typeof variationMap;
   children: ReactNode;
-} & ComponentProps<"button">;
+};
+
+type Link = {
+  isLink?: true;
+} & RemixLinkProps;
 
 type Button = {
+  isLink?: false;
   outline?: boolean;
-  variation: keyof typeof variationMap;
-};
+} & ComponentProps<"button">;
 
 const variationMap = {
   outline: "border-2 border-solid border-green-500 text-green-500",
@@ -19,15 +25,23 @@ const variationMap = {
 } as const;
 
 function Button(
-  { children, variation, className, ...props }: Props,
-  ref: ForwardedRef<HTMLButtonElement>,
+  props: Props,
+  ref: ForwardedRef<HTMLButtonElement & HTMLAnchorElement>,
 ) {
+  const { children, variation, className, isLink } = props;
+
+  const base = `text-center w-full max-w-[175px] rounded-full px-4 py-2 font-bold ${variationMap[variation]} ${OpinionBgColorMap[variation as never]} ${className}`;
+
+  if (isLink) {
+    return (
+      <Link {...props} className={`${base} ${className}`} ref={ref}>
+        {children}
+      </Link>
+    );
+  }
+
   return (
-    <button
-      {...props}
-      ref={ref}
-      className={`w-full max-w-[175px] rounded-full px-4 py-2 font-bold ${variationMap[variation]} ${OpinionBgColorMap[variation as never]} ${className}`}
-    >
+    <button {...(props as Button)} className={`${base} ${className}`} ref={ref}>
       {children}
     </button>
   );
