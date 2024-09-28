609ff86a41d87ed19f8644bee8b0c3ab0fb59db8
RIKKAEBI <135724197+RIKKAEBI@users.noreply.github.com>
2024-09-28 18:53:49 +0000

    🚧 hub

--- app/routes/_pages.home._index/index.tsx
+++ app/routes/_pages.home._index/index.tsx
@@ -1,19 +1,22 @@
-import Card from "~/components/Card";
+import { Link } from "@remix-run/react";
 
 export default function Page() {
   return (
-    <div className="m-4">
-      <Card
-        title="過言では？"
-        description="確かにいい感じだが、完全に習得するのに学習コストがかかるので、すべてのアプリに適応するのは難しいかも"
-        user={{
-          displayID: "",
-          displayName: "山田太郎マン",
-          photoURL:
-            "https://avatars.githubusercontent.com/u/135724197?s=96&v=4",
-        }}
-        opinionStatus="disagree"
-      />
+    <div className="m-4 flex flex-col space-y-8">
+      <p>ダッシュボード的な画面</p>
+      <p>下記のリンクで各々のページに飛べます</p>
+      <Link to="/a" className="underline">
+        投票画面
+      </Link>
+      <Link to="/a/judge" className="underline">
+        賛成反対する画面
+      </Link>
+      <Link to="/a/opinion" className="underline">
+        みんなの意見の画面
+      </Link>
+      <Link to="/a/post" className="underline">
+        意見投稿する画面
+      </Link>
     </div>
   );
 }
