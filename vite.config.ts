29d3da20edfb5aa1848c0eb36297c7b671cab2b7
RIKKAEBI <135724197+RIKKAEBI@users.noreply.github.com>
2024-10-12 11:48:43 +0300

    🔧 setup local ssl server

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,22 @@ import {
   vitePlugin as remix,
   cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
 } from "@remix-run/dev";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  server: {
+    proxy: {},
+    host: "local.kotohiro.com",
+    port: 3000,
+  },
   plugins: [
+    basicSsl({
+      name: "test",
+      domains: ["local.kotohiro.com"],
+      certDir: "./certificates",
+    }),
     remixCloudflareDevProxy(),
     remix({
       future: {
