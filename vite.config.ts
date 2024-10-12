b6b30a9f026fd0dc989866c620d400d4fe3e5f54
RIKKAEBI <135724197+RIKKAEBI@users.noreply.github.com>
2024-10-12 15:58:47 +0300

    📦 remix version up!! 2.12.0 -> 2.13.0

--- vite.config.ts
+++ vite.config.ts
@@ -33,6 +33,9 @@ export default ({ mode }: UserConfig) => {
       remixCloudflareDevProxy(),
       remix({
         future: {
+          v3_singleFetch: true,
+          v3_lazyRouteDiscovery: true,
+          v3_optimizeDeps: true,
           v3_fetcherPersist: true,
           v3_relativeSplatPath: true,
           v3_throwAbortReason: true,
