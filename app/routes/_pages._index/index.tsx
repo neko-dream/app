07be7a3fb5fac434f540cc136809f01ae0927c00
RIKKAEBI <135724197+RIKKAEBI@users.noreply.github.com>
2024-10-13 10:57:21 +0300

    👍 fix top page ui

--- app/routes/_pages._index/index.tsx
+++ app/routes/_pages._index/index.tsx
@@ -13,9 +13,13 @@ export default function Index() {
   };
 
   return (
-    <div className="flex flex-1 flex-col items-center justify-center space-y-4">
-      <GoogleLoginButton onClick={handleClick} />
-      <FacebookLoginButton />
+    <div className="flex flex-1 flex-col items-center ">
+      <p className="mt-8 font-bold">ことひろにログインする</p>
+      <p className="mt-3">打っていいのは打たれる覚悟のある奴だけだ</p>
+      <div className="mt-8 space-y-4">
+        <GoogleLoginButton onClick={handleClick} />
+        <FacebookLoginButton />
+      </div>
     </div>
   );
 }
--- app/routes/_pages/route.tsx
+++ app/routes/_pages/route.tsx
@@ -1,12 +1,19 @@
 import { Outlet } from "@remix-run/react";
-import Layout from "./components/Layout";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Avator from "~/components/Avator";
 
 export default function Route() {
   return (
     <div className="bg-[#e0efff]">
-      <Layout>
+      <div className="mx-auto flex min-h-screen max-w-sm flex-col bg-white pb-16">
+        <div className="flex h-10 w-full items-center justify-between border-b-[1px] border-solid border-[#d6e3ed] px-4">
+          <p>ことひら</p>
+          <Avator src="https://example.com" className="h-8 w-8" />
+        </div>
         <Outlet />
-      </Layout>
+      </div>
+      <ToastContainer position="top-center" />
     </div>
   );
 }
