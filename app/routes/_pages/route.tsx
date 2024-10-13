3814d3c9eab9842b31ff2c268be6c5b838631880
RIKKAEBI <135724197+RIKKAEBI@users.noreply.github.com>
2024-10-13 15:12:00 +0300

    👍 header to be directly under body

--- app/routes/_pages/route.tsx
+++ app/routes/_pages/route.tsx
@@ -18,29 +18,27 @@ export default function Route() {
   const { pathname } = useLocation();
 
   return (
-    <div className="bg-[#e0efff]">
-      <div className="mx-auto flex min-h-screen max-w-sm flex-col bg-white pb-16">
-        <div className="flex h-10 w-full items-center justify-between border-b-[1px] border-solid border-[#d6e3ed] px-4">
-          <Link to={"/"}>Kotohiro</Link>
+    <>
+      <header className="flex h-10 w-full items-center justify-between border-b-[1px] border-solid border-[#d6e3ed] px-4">
+        <Link to={"/"}>Kotohiro</Link>
 
-          {/* MEMO: トップページでは表示させない、ログインボタンが存在するため */}
-          {pathname !== "/" && (
-            <Suspense>
-              <Await resolve={$user}>
-                {(user) => {
-                  if (user?.isVerify) {
-                    return (
-                      <Avator src={user?.iconURL || ""} className="h-8 w-8" />
-                    );
-                  }
-                }}
-              </Await>
-            </Suspense>
-          )}
-        </div>
-        <Outlet />
-      </div>
+        {/* MEMO: トップページでは表示させない、ログインボタンが存在するため */}
+        {pathname !== "/" && (
+          <Suspense>
+            <Await resolve={$user}>
+              {(user) => {
+                if (user?.isVerify) {
+                  return (
+                    <Avator src={user?.iconURL || ""} className="h-8 w-8" />
+                  );
+                }
+              }}
+            </Await>
+          </Suspense>
+        )}
+      </header>
+      <Outlet />
       <ToastContainer position="top-center" />
-    </div>
+    </>
   );
 }
