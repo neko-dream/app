0efbe7a318bc9a2bcab98c87a5ad6df3c486dfbc
RIKKAEBI <135724197+RIKKAEBI@users.noreply.github.com>
2024-10-13 17:29:58 +0300

    👍 storybook as close to production as possible

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,6 @@
 import type { Preview } from "@storybook/react";
-import '../app/tailwind.css';
+import "../app/tailwind.css";
+import "./preview.css";
 
 const preview: Preview = {
   parameters: {
