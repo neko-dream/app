1a0fbe6707dfe7d655873a842337cad7c378032e
RIKKAEBI <135724197+RIKKAEBI@users.noreply.github.com>
2024-10-13 10:53:36 +0300

    👍 adjust api client

--- app/libs/api.ts
+++
@@ -1,4 +0,0 @@
-import createClient from "openapi-fetch";
-import type { paths } from "~/types/openapi";
-
-export const api = createClient<paths>({ baseUrl: API_BASE_URL });
---
+++ app/libs/api/index.ts
@@ -0,0 +1,30 @@
+import createClient from "openapi-fetch";
+import { paths } from "./openapi";
+
+/**
+ * objectをFormDataに変換する
+ */
+const convertFormData = (params: object): FormData => {
+  const formData = new FormData();
+
+  for (const entries of Object.entries(params)) {
+    const value =
+      typeof entries[1] === "number" ? String(entries[1]) : entries[1];
+    formData.append(entries[0], value);
+  }
+
+  return formData;
+};
+
+/**
+ * APIクライアント
+ */
+export const api = createClient<paths>({
+  baseUrl: API_BASE_URL,
+  bodySerializer: (body) => body && convertFormData(body),
+  fetch: (request) => {
+    return globalThis.fetch(request, {
+      mode: "cors",
+    });
+  },
+});
