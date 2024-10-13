8fd3bebc3b0b7ba72f2034b7f963dbfa2d7c6f39
RIKKAEBI <135724197+RIKKAEBI@users.noreply.github.com>
2024-10-13 17:30:46 +0300

    👍 create session

---
+++ app/components/Session/index.stories.tsx
@@ -0,0 +1,46 @@
+import { createRemixStub } from "@remix-run/testing";
+import type { Meta, StoryObj } from "@storybook/react";
+import Session from ".";
+
+const meta: Meta<typeof Session> = {
+  title: "components/Session",
+  component: Session,
+  decorators: [
+    (Story) => {
+      const Stub = createRemixStub([
+        {
+          path: "/",
+          Component: Story,
+        },
+      ]);
+      return <Stub />;
+    },
+  ],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    talkSession: {
+      id: "0000",
+      theme: "レッサーパンダかわいい、アイドルにしたい",
+      owner: {
+        displayID: "西山公園のアイドルレッサーパンダをどうしても推し隊",
+        displayName: "西山公園のアイドルレッサーパンダをどうしても推し隊",
+        iconURL: "https://avatars.githubusercontent.com/u/135724197?s=96&v=4",
+      },
+      createdAt: "2024-10-13T21:38:41Z",
+      finishedAt: "2024-10-13T21:38:41Z",
+      scheduledEndTime: "2024-10-13T21:38:41Z",
+      location: {
+        latitude: 0,
+        longitude: 0,
+        prefecture: "福井県",
+        city: "鯖江市",
+      },
+    },
+    opinionCount: 100,
+  },
+};
---
+++ app/components/Session/index.tsx
@@ -0,0 +1,37 @@
+import { RiMapPinLine } from "react-icons/ri";
+import { paths } from "~/libs/api/openapi";
+import { JST } from "~/libs/date";
+import Avator from "../Avator";
+import OpinionLink from "../OpinionCount";
+
+// FIXME: ジェネリクスを作る
+type Props =
+  paths["/talksessions"]["get"]["responses"]["200"]["content"]["application/json"]["talkSessions"][number];
+
+export default function Session({ talkSession, opinionCount }: Props) {
+  const isFinish = JST(talkSession.finishedAt).isBefore();
+
+  return (
+    <div className="h-[156px] p-4 border-b border-gray-400 bg-white">
+      <p className="text-xs text-gray-500">テーマ</p>
+      <p className="text-[14px] mt-1">{talkSession.theme}</p>
+      <div className="flex space-x-2 items-center mt-1">
+        <Avator
+          src={talkSession.owner.iconURL}
+          className="block shrink-0 h-5 w-5"
+        />
+        <p className="text-gray-500 text-xs">{talkSession.owner.displayName}</p>
+      </div>
+      <p className="text-xs text-white bg-blue-500 rounded-full px-2 py-1 inline-block whitespace-nowrap mt-4">
+        {JST(talkSession.finishedAt).format("MM/DD(ddd)まで")}
+      </p>
+      <div className="flex justify-between mt-2">
+        <div className="flex items-center space-x-1">
+          <RiMapPinLine className="text-gray-500" />
+          <p className="text-xs text-gray-500">{talkSession.location?.city}</p>
+        </div>
+        <OpinionLink count={opinionCount} />
+      </div>
+    </div>
+  );
+}
