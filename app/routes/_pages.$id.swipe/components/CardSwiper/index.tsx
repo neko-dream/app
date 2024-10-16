4878b2227ca9d0c42dd925657012b4a9790cfd4f
RIKKAEBI <135724197+RIKKAEBI@users.noreply.github.com>
2024-10-16 11:25:01 +0300

    🚧 WIP

--- app/routes/_pages.$id.swipe/components/CardSwiper/index.tsx
+++ app/routes/_pages.$id.swipe/components/CardSwiper/index.tsx
@@ -35,17 +35,36 @@ export function Deck() {
   // Create a bunch of springs using the helpers above
   // Create a gesture, we're interested in down-state, delta (current-pos - click-pos), direction and velocity
   const bind = useDrag(
-    ({ args: [index], down, movement: [mx], direction: [xDir], velocity }) => {
-      const trigger = velocity > 0.2; // If you flick hard enough it should trigger the card to fly out
-      const dir = xDir < 0 ? -1 : 1; // Direction should either point left or right
+    ({ args: [index], down, movement: [mx, my], velocity }) => {
+      const trigger = velocity > 0.1; // If you flick hard enough it should trigger the card to fly out
+      // 動きの幅を取得してどちらに飛ばすか決める。
+      let xdir = 0;
+      if (100 < mx) {
+        xdir = 1;
+      } else if (mx < -100) {
+        xdir = -1;
+      }
+
+      let ydir = 0;
+      if (100 < my) {
+        ydir = 1;
+      } else if (my < -100) {
+        ydir = -1;
+      }
+
+      // const xdir = xDir < 0 ? -1 : 1; // Direction should either point left or right
+      // const ydir = yDir < 0 ? -1 : 1; // Direction should either point left or right
       if (!down && trigger) gone.add(index); // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
       api.start((i) => {
         if (index !== i) return; // We're only interested in changing spring-data for the current spring
         const isGone = gone.has(index);
-        const x = isGone ? (200 + window.innerWidth) * dir : down ? mx : 0; // When a card is gone it flys out left or right, otherwise goes back to zero
-        const rot = mx / 100 + (isGone ? dir * 10 * velocity : 0); // How much the card tilts, flicking it harder makes it rotate faster
+        const x = isGone ? (200 + window.innerWidth) * xdir : down ? mx : 0; // When a card is gone it flys out left or right, otherwise goes back to zero
+        const y = isGone ? (200 + window.innerHeight) * ydir : down ? my : 0; // When a card is gone it flys out left or right, otherwise goes back to zero
+        const rot = mx / 100 + (isGone ? xdir * 10 * velocity : 0); // How much the card tilts, flicking it harder makes it rotate faster
         const scale = down ? 1.1 : 1; // Active cards lift up a bit
+
         return {
+          y,
           x,
           rot,
           scale,
