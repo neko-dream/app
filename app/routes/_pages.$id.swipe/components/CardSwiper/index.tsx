0def69e4711d050a382d9bf37c6c62c7a12c4c7d
RIKKAEBI <135724197+RIKKAEBI@users.noreply.github.com>
2024-10-16 12:35:31 +0300

    🚧 削除判定を動かし終わった後に

--- app/routes/_pages.$id.swipe/components/CardSwiper/index.tsx
+++ app/routes/_pages.$id.swipe/components/CardSwiper/index.tsx
@@ -55,12 +55,23 @@ export function Deck() {
 
       // const xdir = xDir < 0 ? -1 : 1; // Direction should either point left or right
       // const ydir = yDir < 0 ? -1 : 1; // Direction should either point left or right
-      if (!down && trigger) gone.add(index); // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
+      // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
+      // console.log(gone.size);
+
       api.start((i) => {
-        if (index !== i) return; // We're only interested in changing spring-data for the current spring
+        if (!down && trigger && (ydir !== 0 || xdir !== 0)) {
+          gone.add(index);
+        }
+
+        if (i !== index) return;
+
         const isGone = gone.has(index);
-        const x = isGone ? (200 + window.innerWidth) * xdir : down ? mx : 0; // When a card is gone it flys out left or right, otherwise goes back to zero
-        const y = isGone ? (200 + window.innerHeight) * ydir : down ? my : 0; // When a card is gone it flys out left or right, otherwise goes back to zero
+        const x = isGone ? (200 + window.innerWidth) * xdir : down ? mx : i * 6; // When a card is gone it flys out left or right, otherwise goes back to zero
+        const y = isGone
+          ? (200 + window.innerHeight) * ydir
+          : down
+            ? my
+            : i * 6; // When a card is gone it flys out left or right, otherwise goes back to zero
         const rot = mx / 100 + (isGone ? xdir * 10 * velocity : 0); // How much the card tilts, flicking it harder makes it rotate faster
         const scale = down ? 1.1 : 1; // Active cards lift up a bit
 
@@ -102,6 +113,9 @@ export function Deck() {
             {...bind(i)}
             style={{
               transform: interpolate([rot, scale], trans),
+              // backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/f/f5/RWS_Tarot_08_Strength.jpg")`,
+              // width: "100px",
+              // height: "200px",
             }}
           >
             <Card
