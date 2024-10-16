f2f9ab41740207c8657197295c0239d7a3ac0001
RIKKAEBI <135724197+RIKKAEBI@users.noreply.github.com>
2024-10-16 07:34:42 +0300

    👍 要素ないでスクロールできるように

--- app/routes/_pages.home._index/index.tsx
+++ app/routes/_pages.home._index/index.tsx
@@ -13,16 +13,17 @@ export default function Page() {
   const isFinished = params.get("q") === "finished";
 
   return (
-    <div className="flex flex-col">
-      <Heading className="h-10">投稿されたセッション</Heading>
+    <>
+      <Heading className="shrink-0 h-10">投稿されたセッション</Heading>
       <Tabs
         items={[
           { label: "開催中", href: "/home" },
           { label: "終了", href: "/home?q=finished" },
         ]}
         active={isFinished ? "終了" : "開催中"}
+        className="shrink-0"
       />
-      <div className="space-y-2 bg-gray-100 h-full pt-2">
+      <div className="space-y-2 bg-gray-100 h-max pt-2 overflow-y-scroll">
         <Suspense>
           <Await resolve={$session}>
             {(data) => {
@@ -39,6 +40,6 @@ export default function Page() {
           </Await>
         </Suspense>
       </div>
-    </div>
+    </>
   );
 }
